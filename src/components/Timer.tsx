17ad852464f6bdb33d42d4ca09362ad20d7cb89d
tylerd <tyler@tdaigle.com>
2023-01-18 04:34:58 -0500

    timer stuff

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
 import { useTimer } from "../hooks/useTimer";
 
+const padTime = (time: number): string =>
+  time < 10 ? `0${time}` : time.toString();
+
 export default function Timer() {
-  const seconds = useTimer();
+  let currentSeconds = useTimer();
+  const hours = Math.floor(currentSeconds / (60 * 60));
+
+  currentSeconds = currentSeconds % (60 * 60);
+  const minutes = Math.floor(currentSeconds / 60);
+
+  currentSeconds = currentSeconds % 60;
+  const seconds = currentSeconds;
+
   return (
     <div>
-      <span>{seconds}</span>
+      <h1>Here is the time:</h1>
+      <span>{padTime(hours)}</span>:<span>{padTime(minutes)}</span>:
+      <span>{padTime(seconds)}</span>
     </div>
   );
 }
--- src/hooks/useTimer.tsx
+++ src/hooks/useTimer.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export function useTimer() {
-  const [elapsedSeconds, setElapsedSeconds] = useState(0);
+  const [elapsedSeconds, setElapsedSeconds] = useState(342);
 
   useEffect(() => {
     let lastUpdate = new Date();
@@ -9,14 +9,20 @@ export function useTimer() {
     const timerId = setInterval(() => {
       const now = new Date();
       let diff = now.getSeconds() - lastUpdate.getSeconds();
+
+      // when the seconds roll back to 0, we will get a negative difference which we don't want.
+      // It will be 0 - 59, not 60 - 59.
       if (diff < 0) {
-        diff = 1;
+        diff = 1; //
       }
+
       if (diff >= 1) {
         setElapsedSeconds((s) => s + diff);
       }
     }, 500);
-    return () => clearInterval(timerId);
+    return () => {
+      clearInterval(timerId);
+    };
   }, [elapsedSeconds]);
 
   return elapsedSeconds;
